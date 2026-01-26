e1285f52cce727539083261b18f6de913e4e8481
mohamed izeldeen <eng.mohamed.izeldeen@gmail.com>
2026-01-27 02:52:06 +0400

    add

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>تسجيل جديد - ضحي</title>
+     <title>NLD - Salon</title>
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="{{ asset('images/fav.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">

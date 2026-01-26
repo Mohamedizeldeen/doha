f3fd6a36fd2eb622eb713afecbadc440492e149c
mohamed izeldeen <eng.mohamed.izeldeen@gmail.com>
2026-01-27 02:42:36 +0400

    add fav

--- resources/views/superAdmin/layout/app.blade.php
+++ resources/views/superAdmin/layout/app.blade.php
@@ -4,7 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'NLD - Salon') }}</title>
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="{{ asset('images/fav.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
